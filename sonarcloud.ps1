ecf42f019c4d7f70008fc6a0aac50d5f48e9c2e5
knsatishkumar <kn.satishkumar@gmail.com>
2022-06-25 04:26:51 +0530

    git actions

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -23,12 +23,7 @@ Write-Host "branch is $branch"
 
 dotnet tool restore
 #dotnet tool run dotnet-sonarscanner begin '
-.\.sonar\scanner\dotnet-sonarscanner begin '
-  /k:"alkampfergit_DotNetCoreCryptography" ' # Key of the project
-  /v:"$assemblyVer" '                        # Version of the assemly as calculated by gitversion
-  /o:"alkampfergit-github" '                 # account
-  /d:sonar.login="$sonarSecret" '            # Secret
-  /d:sonar.host.url="https://sonarcloud.io" '
+.\.sonar\scanner\dotnet-sonarscanner begin /k:"knsatishkumar-org_demo-sonar" /o:"knsatishkumar-org" /d:sonar.login="$sonarSecret" /d:sonar.host.url="https://sonarcloud.io" '
   /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx ' # Path where I'm expecting to find test result in trx format
   /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml ' # Name of the code coverage file
   /d:sonar.coverage.exclusions="**Test*.cs" '   # asembly names to be excluded from code coverage
@@ -39,7 +34,7 @@ dotnet build src --configuration release
 
 # Now execute tests with special attention to produce output
 # that can be easily read by SonarCloud analyzer
-dotnet test "./src/DotNetCoreCryptography.Tests/DotNetCoreCryptography.Tests.csproj" '
+dotnet test '
   -collect:"XPlat Code Coverage" '           # cross platform code coverage
   -results-directory TestResults/ '          # Test Result directory
   -logger "trx;LogFileName=unittests.trx" '  # Use standard trx format for logger output
